35be10ef41653bbb946633964cc98c12729ac268
han <iwas1006@gmail.com>
2019-10-05 00:48:14 +0900

    modify

--- rtmBot/izyrtm_prop.py
+++ rtmBot/izyrtm_prop.py
@@ -2,6 +2,7 @@ domain='https://monbot.hopto.org'
 apm_id='admin'
 apm_pw='New1234!'
 apm_url='https://monbot.hopto.org:3000/login'
+snapshot_url='https://monbot.hopto.org:3000'
 
 db_host='monbot.hopto.org'
 db_user='izyrtm'
